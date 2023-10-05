dcdd125f24afa357ce993e82f228873482eeaf70
RaonyM <112267602+RaonyM@users.noreply.github.com>
2023-10-05 17:47:00 -0300

    Create class.Consulta.php

---
+++ classes/class.Consulta.php
@@ -0,0 +1,76 @@
+    <?php 
+
+class Consulta {
+
+    private $paciente, $dentistaExe, $dataC, $horarioC, $duracaoC, $procedimentoC;
+
+    public function __construct($paciente, $dentistaExe, $dataC, $horarioC, $duracaoC, $procedimentoC)
+    {
+        $this->paciente = $paciente;
+        $this->dentistaExe = $dentistaExe;
+        $this->dataC = $dataC;
+        $this->horarioC = $horarioC;
+        $this->duracaoC = $duracaoC;
+        $this->procedimentoC = $procedimentoC;
+    }
+
+    public function getPaciente()
+    {
+      return $this->paciente;
+    }
+
+    public function setPaciente ($paciente)
+    {
+      $this->paciente = $paciente;
+    }
+    
+    public function getDentistaExe()
+    {
+      return $this->dentistaEX;
+    }
+
+    public function setDentistaExe ($dentistaExe)
+    {
+      $this->setDentistaExe = $setDentistaExe;
+    }
+
+    public function getDataC()
+    {
+      return $this->dataC;
+    }
+
+    public function setDataC ($dataC)
+    {
+      $this->dataC = $dataC;
+    }
+
+    public function getHorarioC()
+    {
+      return $this->horarioC;
+    }
+
+    public function setHorarioC ($horarioC)
+    {
+      $this->horarioC = $horarioC;
+    }
+  
+    public function getDuracaoC()
+    {
+      return $this->duracaoC;
+    }
+
+    public function setDuracaoC ($duracaoC)
+    {
+      $this->duracaoC = $duracaoC;
+    }
+  
+    public function getProcedimentoC()
+    {
+      return $this->$procedimentoC;
+    }
+
+    public function setProcedimentoC ($procedimentoC)
+    {
+      $this->$procedimentoC = $procedimentoC;
+    }
+}
