c9d2bdfc9e82cd4da983252e601120050494f2fc
RaonyM <112267602+RaonyM@users.noreply.github.com>
2023-10-26 20:22:19 -0300

    Update class.Dentista.php
    Sprint 4

--- classes/class.Dentista.php
+++ classes/class.Dentista.php
@@ -5,15 +5,17 @@ include_once('../global.php');
 class Dentista extends Pessoa {
 
     protected $CRO;
+    protected $endereco;
     static $local_filename = "Dentistas.txt";
   
-    public function __construct($nome, $RG, $email, $telefone, $CRO) 
+    public function __construct($nome, $RG, $email, $telefone, $CRO, $endereco) 
     {
         $this->nome=$nome;
         $this->RG=$RG;
         $this->email=$email;
         $this->telefone=$telefone;
         $this->CRO=$CRO;
+        $this->endereco=$endereco;
     }
 
     public function getCRO ()
